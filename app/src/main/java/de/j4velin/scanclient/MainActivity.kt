07304343a76a31c7788b37e0d0cf8c086267f796
Thomas Hoffmann <git@j4velin.de>
2025-08-18 21:13:59 +0200

    add support for batch-prompt

--- app/src/main/java/de/j4velin/scanclient/MainActivity.kt
+++ app/src/main/java/de/j4velin/scanclient/MainActivity.kt
@@ -10,12 +10,17 @@ import android.text.InputType
 import android.view.KeyEvent
 import android.view.View
 import android.view.inputmethod.InputMethodManager
+import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import androidx.core.view.isVisible
 import java.net.URL
 
 
 class MainActivity : Activity() {
+
+    var currentPage = 1
+    var totalPages = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -61,7 +66,20 @@ class MainActivity : Activity() {
     }
 
     fun scan(view: View) {
-        val progressDialog = ProgressDialog.show(this, "Scanning", "Please wait...", true, false)
+        view.isVisible = false
+        val progressDialog: ProgressDialog?
+        currentPage = 1
+        totalPages = findViewById<EditText>(R.id.pages).text.toString().toInt()
+        if (totalPages > 1) {
+            progressDialog = null
+            findViewById<Button>(R.id.next).apply {
+                isVisible = true
+                text = "$currentPage / $totalPages"
+            }
+            Toast.makeText(this, "Scanning...", Toast.LENGTH_SHORT).show()
+        } else {
+            progressDialog = ProgressDialog.show(this, "Scanning", "Please wait...", true, false)
+        }
         Thread {
             val result = try {
                 URL("http://${findViewById<EditText>(R.id.ip).text}:8080/${findViewById<EditText>(R.id.pages).text}").readText()
@@ -70,8 +88,32 @@ class MainActivity : Activity() {
                 "Error trying to scan"
             }
             runOnUiThread {
-                progressDialog.dismiss()
+                progressDialog?.dismiss()
                 Toast.makeText(this, result, Toast.LENGTH_LONG).show()
+                if (currentPage == totalPages) view.isVisible = true
+            }
+        }.start()
+    }
+
+    fun next(view: View) {
+        view.isVisible = false
+        Toast.makeText(this, "next", Toast.LENGTH_SHORT).show()
+        Thread {
+            val result = try {
+                URL("http://${findViewById<EditText>(R.id.ip).text}:8080/next").readText()
+            } catch (e: Exception) {
+                e.printStackTrace()
+                "Error trying to scan"
+            }
+            currentPage++
+            (view as Button).text = "$currentPage / $totalPages"
+            runOnUiThread {
+                Toast.makeText(this, result, Toast.LENGTH_SHORT).show()
+                if (currentPage == totalPages) {
+                    findViewById<View>(R.id.scan).isVisible = true
+                } else {
+                    view.isVisible = true
+                }
             }
         }.start()
     }
